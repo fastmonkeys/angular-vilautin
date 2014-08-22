959be89cd139f236b0898c02f48e58faf8b7b305
Roope Hovi <roope.hovi@gmail.com>
2014-08-22 15:05:51 +0300

    Add build tasks and configuration

---
+++ Gruntfile.js
@@ -0,0 +1,126 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  require('time-grunt')(grunt);
+
+  var appConfig = {
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+
+    yeoman: appConfig,
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          'src/{,*/}*.js'
+        ]
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/spec/{,*/}*.js']
+      }
+    },
+
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= yeoman.dist %>/{,*/}*',
+            '!<%= yeoman.dist %>/.git*'
+          ]
+        }]
+      }
+    },
+
+    uglify: {
+       dist: {
+         files: {
+           '<%= yeoman.dist %>/js/britney.min.js': [
+             'src/britney.js',
+             'src/notification-service.js',
+             'src/notification-controller.js'
+           ]
+         }
+       }
+    },
+
+    less: {
+      development: {
+        files: {
+          '<%= yeoman.dist %>/css/britney.css': ' styles/britney.less'
+        }
+      }
+    },
+
+    cssmin: {
+      default: {
+        files: {
+          '<%= yeoman.dist %>/css/britney.min.css': '<%= yeoman.dist %>/css/britney.css'
+        }
+      }
+    },
+
+    concat: {
+      dist: {
+        src: ['src/britney.js', 'src/notification-service.js', 'src/notification-controller.js'],
+        dest: '<%= yeoman.dist %>/js/britney.js'
+      }
+    },
+
+    copy: {
+      less: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: 'styles/',
+            dest: '<%= yeoman.dist %>/less',
+            src: [
+              'britney.less'
+            ]
+          }
+        ]
+      }
+    },
+
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js',
+        singleRun: true
+      }
+    }
+  });
+
+  grunt.registerTask('test', [
+    'karma'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'less',
+    'cssmin',
+    'uglify',
+    'concat',
+    'copy:less'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'test',
+    'build'
+  ]);
+};
---
+++ test/karma.conf.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+    basePath: '../',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/underscore/underscore.js',
+      'src/**/*.js',
+      'test/**/*_spec.js'
+    ],
+
+    // web server port
+    port: 8080,
+
+    browsers: [
+      'PhantomJS'
+    ],
+
+    // Which plugins to enable
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-jasmine'
+    ],
+
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO
+  });
+};
