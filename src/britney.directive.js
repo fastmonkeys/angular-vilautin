20c56af9ef8008d099c97013237b43cd89005721
Roope Hovi <roope.hovi@gmail.com>
2015-02-26 23:17:37 +0200

    Remove unnecessary wrapper div from directive template

--- src/britney.directive.js
+++ src/britney.directive.js
@@ -3,14 +3,12 @@ angular.module('britney')
   .directive('britneyNotifications', function () {
     return {
       template:
-      '<div class="britney-notifications">' +
         '<div ' +
           'ng-repeat="(id, notification) in ctrl.notifications" ' +
           'class="notification {{notification.severity}}" ' +
           'ng-click="ctrl.removeNotification(id)">' +
             '<span class="message">{{ notification.message }}</span>' +
-        '</div>' +
-      '</div>',
+        '</div>',
       restrict: 'E',
       controller: 'BritneyController',
       controllerAs: 'ctrl'
